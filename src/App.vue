bba0ee8089859b087c50892c24c984418b191f1c
shuaige <L13164621205.com>
2022-04-06 21:02:10 +0800

    hh

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+<h1>黑马头条</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Appaaa'
+}
+</script>
+
+<style lang="less">
+
+</style>
